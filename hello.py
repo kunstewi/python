5eb152cf2d3ed546f1868e3f71806126eaf1a206
kunstewi <kan.ai35423@gmail.com>
2026-01-19 23:39:38 +0530

    init

---
+++ hello.py
@@ -0,0 +1,12 @@
+# # if 5 > 2:
+# #     print("5 is greater than 2")
+
+# # x = "Hello World"
+
+# # # This is a comment in python
+
+# print("Hello World", end=" ")
+# print("my friend")
+
+# print("I am", 35, "Years Old")
+
